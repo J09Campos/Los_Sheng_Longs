9dbd6ee0f07493f6205118a76054692862ec0184
IFFernandez <isaacferbre2015@gmail.com>
2025-10-02 10:19:07 -0600

    Proyecto en Reloj_Marcador.Services
    Se implementaron las siguientes clases en Reloj_Marcador.Services:
    
    Se creo la Carpeta Abstract y su contenido es ILoginService
    
    Y Login Service por fuera de la carpeta

---
+++ Reloj_Marcador.Services/Abstract/ILoginService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reloj_Marcador.Services.Abstract
+{
+    public interface ILoginService
+    {
+
+        Task<(string Mensaje, string? NombreCompleto)> LoginAsync(string usuario, string contrasena);
+
+
+    }
+}
---
+++ Reloj_Marcador.Services/LoginService.cs
@@ -0,0 +1,38 @@
+﻿using Reloj_Marcador.Repository;
+using Reloj_Marcador.Services.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reloj_Marcador.Services
+{
+    public class LoginService : ILoginService
+    {
+
+        private readonly LoginRepository _loginRepository;
+
+        public LoginService(LoginRepository loginRepository)
+        {
+            _loginRepository = loginRepository;
+        }
+
+        public async Task<(string Mensaje, string? NombreCompleto)> LoginAsync(string usuario, string contrasena)
+        {
+
+            
+            if (string.IsNullOrWhiteSpace(usuario) || string.IsNullOrWhiteSpace(contrasena))
+            {
+                return ("Usuario y/o contraseña incorrectos.", null);
+            }
+
+            
+            var resultado = await _loginRepository.LoginAsync(usuario, contrasena);
+
+            
+            return resultado;
+        }
+
+    }
+}
