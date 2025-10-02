1d4e55e8215fba1961c3f2ad111d401dccf2e961
IFFernandez <isaacferbre2015@gmail.com>
2025-10-02 10:12:50 -0600

    Clases en Reloj_Marcador.Repository
    Se hizo la implementación de las clases:
    
    DbConnectionFactory
    IDbConnectionFactory
    LoginRepository

---
+++ Reloj_Marcador.Repository/IDbConnectionFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reloj_Marcador.Repository
+{
+    public interface IDbConnectionFactory
+    {
+
+        IDbConnection CreateConnection();
+
+
+    }
+}
